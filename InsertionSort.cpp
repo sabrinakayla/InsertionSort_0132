736e47a62ec201f531177646a3996ea01c7995c8
sabrinakayla <sabrinakuliah10@gmail.com>
2026-03-12 10:48:07 +0700

    prosedur input 1

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input (){
+    while (true){
+        cout << "Masukkan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
+}
